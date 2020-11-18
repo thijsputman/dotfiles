3775b4b0a15a9ed1f65ddc4da195429340087d84
Thijs Putman <thijs@sgraastra.net>
2020-11-18 19:23:51 +0100

    Add SNMP temperature reading for Raspberry Pi 4
    Not the most beautiful solution, but functional for now.

---
+++ scripts/pi4-cpu-temp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Gave up on trying to get snmpwalk to work properly; the below only returns
+# when retrieved through snmpget – something to solve another time...
+
+# For good measure: Copy to /usr/local/sbin/cputemp.sh and run from there
+
+if [ "$1" = "-g" ] && [ "$2" = ".1.3.6.1.4.1.8072.9999.9999.314.1" ] ; then
+  echo .1.3.6.1.4.1.8072.9999.9999.314.1
+  echo gauge
+  cat /sys/class/thermal/thermal_zone0/temp
+fi
