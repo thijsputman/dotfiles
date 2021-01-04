f172871abfc2463a4ba693494a382be5a3fe575e
Thijs Putman <thijs@sgraastra.net>
2021-01-04 18:45:20 +0100

    Switch from screenfetch to neofetch
    Seems to be more actively maintained than screenfetch.
    
    And instead of awkwardly hacking it into Ubuntu's MoTD-service, run it
    as part of the regular (i.e. user) Bash-scripts. More standard, easier
    to modify/maintain _and_ it now displays stuff (e.g. X11/VcXsrv) that's
    initialised as part of my user's session.

--- scripts/init-pi4.sh
+++ scripts/init-pi4.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+ln -s .config/neofetch/config-pi4.conf ~/.config/neofetch/config.conf
+
+# .bashrc.d
+
+ln -s .bashrc.d ~/.bashrc.d
+
 chmod +x ~/.bashrc.d/01-pi4 \
   ~/.bashrc.d/20-gpg-pi4
 
-chmod -x ~/.bashrc.d/20-gpg \
-  ~/.bashrc.d/40-path \
-  ~/.bashrc.d/99-wsl
+chmod -x ~/.bashrc.d/01-wsl \
+  ~/.bashrc.d/20-gpg \
+  ~/.bashrc.d/40-path
---
+++ scripts/init-wsl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s .config/neofetch/config.conf ~/.config/neofetch/config.conf
+
+# .bashrc.d
+
+ln -s .bashrc.d ~/.bashrc.d
