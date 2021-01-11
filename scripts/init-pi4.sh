048c2bb57dbff3c86bd74faec23904041f257da4
Thijs Putman <thijs@sgraastra.net>
2021-01-11 19:13:47 +0100

    Further flesh out Pi4 initialisation

--- scripts/init-pi4.sh
+++ scripts/init-pi4.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
 if [ ! -e ~/.config/neofetch/config.conf ]; then
+  mkdir -p ~/.config/neofetch
   ln -s -r "$PWD/.config/neofetch/config-pi4.conf" \
     ~/.config/neofetch/config.conf
 fi
 
+if [ ! -e ~/.inputrc ]; then
+  ln -s -r "$PWD/.inputrc" ~/.inputrc
+fi
+
+if [ ! -e ~/.nanorc ]; then
+  ln -s -r "$PWD/.nanorc" ~/.nanorc
+fi
+
 # .bashrc.d
+
+if [ ! -e ~/.bashrc ]; then
+  ln -s -r "$PWD/.bashrc" ~/.bashrc
+fi
 
 if [ ! -e ~/.bashrc.d ]; then
   ln -s -r "$PWD/.bashrc.d" ~/.bashrc.d
@@ -13,11 +26,27 @@ fi
 
 if [ -d ~/.bashrc.d ] && [ -L ~/.bashrc.d ]; then
 
-  chmod +x ~/.bashrc.d/01-pi4 \
-    ~/.bashrc.d/20-gpg-pi4
+  chmod +x ~/.bashrc.d/05-pi4 \
+    ~/.bashrc.d/20-gpg-pi4 \
+    ~/.bashrc.d/30-x11-pi4
 
-  chmod -x ~/.bashrc.d/01-wsl \
-    ~/.bashrc.d/20-gpg \
-    ~/.bashrc.d/40-path
+  chmod -x ~/.bashrc.d/05-wsl \
+    ~/.bashrc.d/05-wsl-ip \
+    ~/.bashrc.d/20-gpg-wsl \
+    ~/.bashrc.d/30-x11-wsl
 
+fi
+
+# Git
+
+if [ ! -e ~/.gitattributes_global ]; then
+  ln -s -r "$PWD/.gitattributes_global" ~/.gitattributes_global
+fi
+
+if [ ! -e ~/.gitconfig ]; then
+  ln -s -r "$PWD/.gitconfig" ~/.gitconfig
+fi
+
+if [ ! -e ~/.gitignore_global ]; then
+  ln -s -r "$PWD/.gitignore_global" ~/.gitignore_global
 fi
