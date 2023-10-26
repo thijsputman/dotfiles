97b4e8624417aa7aed79f68f8c57edbe806ddd74
Thijs Putman <thijs@sgraastra.net>
2023-10-26 13:31:28 +0200

    Various pyenv enhancements
    With a recent update, the initialisation procedure changed slightly;
    updated the .bashrc.d script accordingly.
    
    Add a pyenv update-check (via a scheduled anacron task) that notifies
    if/when updates are available.

---
+++ scripts/pyenv-update-check.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ -n "$PYENV_ROOT" ]; then
+
+  : > ~/.pyenv-update
+
+  cd "$PYENV_ROOT" || exit
+  git remote update
+
+  if [[ $(git rev-parse HEAD) != $(git rev-parse '@{u}') ]]; then
+
+    status=$(git status | head -n 2 | tail -n 1)
+
+    if [[ $status == *behind* ]]; then
+      echo "${status/Your branch/pyenv}" >> ~/.pyenv-update
+      printf 'To upgrade run: pyenv update' >> ~/.pyenv-update
+    fi
+
+  fi
+
+fi
