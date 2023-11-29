6a4259f78f5d87131b45bd8abdfc9f4f8dacdbca
Thijs Putman <thijs@sgraastra.net>
2023-11-29 12:21:51 +0100

    Backup ESPHome configuration
    Recently moved the container over from one of the RPIs to my laptop as
    there appears to be some issues with building compiling ESP32 binaries
    on ARM64. As such the configuration now needs to be backed up from my
    laptop.

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -11,3 +11,21 @@ rclone sync ~/.gnupg/pubring.kbx onedrive:AppData/WSL/.gnupg
 rclone sync ~/.gnupg/trustedkeys.kbx onedrive:AppData/WSL/.gnupg
 rclone sync ~/.ssh/config onedrive:AppData/WSL/.ssh
 rclone sync ~/.bash_history onedrive:AppData/WSL/
+
+# Backup ESPHome configuration
+
+rclone sync ~/esphome/config --max-depth 0 --exclude secrets.yaml \
+  onedrive:AppData/WSL/esphome/
+
+# Remove all WPA-keys prior to uploading secrets.yaml – bit crude: The file is
+# copied unconditionally (there doesn't appear to be an rclone-"native" way to
+# compare modifications times)...
+# We do set the remote modification time to the correct/actual time, so
+# practically there's no impact (apart from superfluous uploads).
+sed -E 's/^(wifi_[a-z0-9_]+:).*/\1/Ig' ~/esphome/config/secrets.yaml |
+  rclone rcat onedrive:AppData/WSL/esphome/secrets.yaml
+rclone touch onedrive:AppData/WSL/esphome/secrets.yaml -t \
+  "$(
+    date -u +"%Y-%m-%dT%H:%M:%S" -d \
+      @"$(stat -c %Y ~/esphome/config/secrets.yaml)"
+  )"
