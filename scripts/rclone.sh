7fc646f6b69a72334b7bc0ecdfb43576f0d2ca39
Thijs Putman <thijs@sgraastra.net>
2024-01-24 10:16:45 +0100

    Also backup ESPHome's "docker-compose.yml"
    Simply start the backup one folder-level higher...

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -14,7 +14,7 @@ rclone sync ~/.bash_history onedrive:AppData/WSL/
 
 # Backup ESPHome configuration
 
-rclone sync ~/esphome/config --max-depth 0 --exclude secrets.yaml \
+rclone sync ~/esphome --max-depth 1 --exclude secrets.yaml \
   onedrive:AppData/WSL/esphome/
 
 # Remove all WPA-keys prior to uploading secrets.yaml – bit crude: The file is
@@ -23,8 +23,8 @@ rclone sync ~/esphome/config --max-depth 0 --exclude secrets.yaml \
 # We do set the remote modification time to the correct/actual time, so
 # practically there's no impact (apart from superfluous uploads).
 sed -E 's/^(wifi_[a-z0-9_]+:).*/\1/Ig' ~/esphome/config/secrets.yaml |
-  rclone rcat onedrive:AppData/WSL/esphome/secrets.yaml
-rclone touch onedrive:AppData/WSL/esphome/secrets.yaml -t \
+  rclone rcat onedrive:AppData/WSL/esphome/config/secrets.yaml
+rclone touch onedrive:AppData/WSL/esphome/config/secrets.yaml -t \
   "$(
     date -u +"%Y-%m-%dT%H:%M:%S" -d \
       @"$(stat -c %Y ~/esphome/config/secrets.yaml)"
