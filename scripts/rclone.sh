84fd8614878c6beec48e7dbd66a7c050db194fd9
Thijs Putman <thijs@sgraastra.net>
2024-02-20 01:31:39 +0100

    Symlink SSH configuration from OneDrive
    Now that my D-drive is mounted with a proper fmask and metadata, it's
    possible to symlink the SSH configuration from OneDrive (instead of
    just backing it up there via rclone).
    
    This is more convenient (as from time to time it's easy to modify it
    from Windows) and more consistent (as Windows' SSH configuration is
    also symlinked from OneDrive).

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -2,22 +2,16 @@
 
 set -euo pipefail
 
-# Backup a handful of files that can't (sensibly) by symlinked from OneDrive's
-# location in Windows (mainly due to access right restrictions) and/or where
-# it's not smart to allow updates from OneDrive to be used without manual
-# intervention...
-
 rclone sync ~/.gnupg/pubring.kbx onedrive:AppData/WSL/.gnupg
 rclone sync ~/.gnupg/trustedkeys.kbx onedrive:AppData/WSL/.gnupg
-rclone sync ~/.ssh/config onedrive:AppData/WSL/.ssh
+
 rclone sync ~/.bash_history onedrive:AppData/WSL/
 
 # Backup (personal) Git configuration
 rclone sync ~/ onedrive:AppData/WSL/ --max-depth 1 --skip-links \
   --include "/.gitconfig*"
 
-# Backup some files from ~/.config
-
+# Backup htop- and btop-configuration
 rclone sync ~/.config onedrive:AppData/WSL/.config --max-depth 2 --skip-links \
   --include "/{b,h}top/*"
 
