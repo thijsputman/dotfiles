7eb102273e6e93182aff6d33dfab7ea48937b55e
Thijs Putman <thijs@sgraastra.net>
2024-08-25 16:12:40 +0200

    Simplify and generalise install-script
    First (optional) argument is now the parts-directory from where to run
    install-parts. If not specified, defaults to "config.d".
    
    Removed (ie, simplified) the distinction between installing a single
    part and installing all (executable) parts from a folder. Should this
    ever be needed again (primarily for debugging), I'll include a switch
    to fallback to "interactive" mode.

--- install/install.sh
+++ install/install.sh
@@ -5,38 +5,58 @@ set -euo pipefail
 
 base=$(dirname "$(realpath "$(dirname "${BASH_SOURCE[0]}")")")
 
-sudo -v
+# Sort out which "parts"-directory to use
 
-for file in "$base"/install/parts.d/**; do
+if [[ ! -v 1 ]]; then
+  set -- config.d
+fi
+
+if [[ ! -d "$base"/install/"${1##*/}" ]]; then
+  echo "⛔ Invalid parts-directory \"${1##*/}\" specified..." >&2
+  exit 1
+fi
+
+folder=${1##*/}
+shift
+
+# Validate additional requirements for "bins.d"-parts
+
+if [[ $folder == bins.d && ! $PATH =~ (^|:)"$HOME/.local/bin"(:|$) ]]; then
+
+  if [[ ! -d "$HOME/.local/bin" ]]; then
+    mkdir -p "$HOME/.local/bin"
+  fi
+
+  echo "⚠️ Add \"📂 $HOME/.local/bin\" to PATH... "
+  PATH="$PATH:$HOME/.local/bin"
+
+fi
+
+# Pre-empt sudo for software installation (almost always required)
+if [[ $folder == software.d ]]; then
+  sudo -v
+fi
+
+for file in "$base"/install/"$folder"/**; do
 
   file_basename="$(basename "$file")"
 
+  # On _exact_ filename match (ie, a single file was specified), source and
+  # run the single file; otherwise run all (executable) files
+
   if [[ (-x $file && ! -v 1) || (-v 1 && "$file_basename" =~ ^$1$) ]]; then
 
-    # On _exact_ filename match (ie, a single file was specified), source and
-    # run it interactively
+    echo "📄 $folder/$file_basename"
 
-    if [[ -v 1 && "$file_basename" == "$1" ]]; then
+    # Capture stdout and stderr – only display them if the script fails. fd 3
+    # Is used to relay (non-fatal) warnings/notices and is thus passed through
+    # to the terminal regardless.
 
-      if ! source "$file" 3>&1; then
-        exit 1
-      fi
-
-    # Otherwise, capture stdout and stderr – only display them if the script
-    # fails. fd 3 Is used to relay (non-fatal) warnings/notices and is thus
-    # passed through to the terminal regardless.
-
-    else
-
-      echo "📄 $file_basename"
-
-      if ! { output=$(source "$file" 2>&1); } 3>&1; then
-        echo "$output"
-        printf '\n'
-        echo "⛔ Install failed – see preceding output for details..." >&2
-        exit 1
-      fi
-
+    if ! { output=$(source "$file" 2>&1); } 3>&1; then
+      echo "$output"
+      printf '\n'
+      echo "⛔ Part failed – see preceding output for details..." >&2
+      exit 1
     fi
 
   fi
